2f6a0474a06bce2e3c061ceb52616cafc562451b
kitkat <mainist@skkukdp.re.kr>
2024-09-26 10:29:05 +0900

    username 로그인 유저 정보 받아와서 입력하는 방식으로 수정

--- src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
+++ src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
@@ -1,5 +1,14 @@
 package com.mysite.sbb.UserMatching;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +16,29 @@ import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserMatchingController {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public UserMatchingController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/matching")
-    public String showMatchingPage(Model model) {
-        RestTemplate restTemplate = new RestTemplate();
-        String formHtml = restTemplate.getForObject("http://localhost:8000/matching-form", String.class);
+    public String showMatchingPage(Model model, Principal principal) {
+        String username = principal.getName();
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("user_id", username);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(userData, headers);
+
+        String formHtml = restTemplate.postForObject("http://localhost:8000/matching-form", request, String.class);
+
         model.addAttribute("matchingForm", formHtml);
         return "UserMatchingPage";
     }
