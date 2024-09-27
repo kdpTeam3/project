0e26c8ea907b269b20e28b2bf792a10e026047ed
HyeonSeok <k65654@gmail.com>
2024-09-27 12:43:13 +0900

    유저프로필(음식 선호카테고리)페이지 구현 완료

--- src/main/java/com/mysite/sbb/food/RecommendController.java
+++ src/main/java/com/mysite/sbb/food/RecommendController.java
@@ -2,7 +2,6 @@ package com.mysite.sbb.food;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -23,38 +22,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 public class RecommendController {
 
-    @Autowired
-    private UserProfileService userProfileService;
-
     private final RestTemplate restTemplate = new RestTemplate();
     private final String FLASK_BASE_URL = "http://127.0.0.1:5000"; // Flask 서버 주소
 
-    // GET 요청을 처리하는 메서드 추가
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/recommend")
-    public String showRecommendationPage() {
-        return "food_recommend";  // 추천 페이지 템플릿을 반환
-    }
 
-    // user_id 확인 후 페이지 리다이렉트
+    // 프로필 여부 확인 후 페이지 리다이렉트
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/check_profile")
     public String checkProfileAndRedirect(@RequestParam("user_id") String userId) {
+        // Flask로 유저 프로필 존재 여부 확인 요청
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         String requestBody = "user_id=" + userId;
         HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
 
-        // Flask 서버로 요청 보내서 프로필 확인
         ResponseEntity<String> response = restTemplate.postForEntity(FLASK_BASE_URL + "/check_profile", request, String.class);
 
-        // JSON 데이터를 Map으로 변환
+        // JSON 데이터를 Java Map으로 변환
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            Map<String, Object> result = objectMapper.readValue(response.getBody(), Map.class);
+            Map<String, Object> result = objectMapper.readValue(response.getBody(), new TypeReference<Map<String, Object>>() {});
             boolean profileExists = (boolean) result.get("profile_exists");
 
-            // 프로필이 존재하면 추천 페이지로, 없으면 프로필 입력 페이지로 리다이렉트
+            // 프로필 존재 여부에 따라 페이지 리다이렉트
             if (profileExists) {
                 return "redirect:/food/recommend";
             } else {
@@ -64,6 +54,13 @@ public class RecommendController {
             e.printStackTrace();
             return "redirect:/error";
         }
+    }
+
+    // GET 요청을 처리하는 메서드 추가
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/recommend")
+    public String showRecommendationPage() {
+        return "food_recommend";  // 추천 페이지 템플릿을 반환
     }
 
     @PreAuthorize("isAuthenticated()")
