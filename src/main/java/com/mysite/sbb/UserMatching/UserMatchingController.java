0fa41235aaef3514e318f6742e71964c027ac104
kitkat <mainist@skkukdp.re.kr>
2024-10-01 17:42:49 +0900

    매칭 서비스 탭 로그인 시에만 보이도록 @PreAuthorize("isAuthenticated()") 추가

--- src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
+++ src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
@@ -1,13 +1,5 @@
 package com.mysite.sbb.UserMatching;
 
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,30 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class UserMatchingController {
-
-    private final RestTemplate restTemplate;
-
-    @Autowired
-    public UserMatchingController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    @PreAuthorize("isAuthenticated()")
     @GetMapping("/matching")
-    public String showMatchingPage(Model model, Principal principal) {
-        String username = principal.getName();
-
-        Map<String, String> userData = new HashMap<>();
-        userData.put("user_id", username);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(userData, headers);
-
-        String formHtml = restTemplate.postForObject("http://localhost:8000/matching-form", request, String.class);
-
+    public String showMatchingPage(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        String formHtml = restTemplate.getForObject("http://localhost:8000/matching-form", String.class);
         model.addAttribute("matchingForm", formHtml);
         return "UserMatchingPage";
     }
