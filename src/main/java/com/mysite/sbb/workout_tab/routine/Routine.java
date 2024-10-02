442e06c1a2fc47901454738161f5ba77f78d94fb
ojr <asdasd031@naver.com>
2024-10-02 12:44:26 +0900

    my_goal 만드는중

--- src/main/java/com/mysite/sbb/workout_tab/routine/Routine.java
+++ src/main/java/com/mysite/sbb/workout_tab/routine/Routine.java
@@ -2,36 +2,50 @@ package com.mysite.sbb.workout_tab.routine;
 
 import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.workout_tab.workout.Workout;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
 import java.util.List;
+import lombok.Data;
 
 
 @Data
 @Entity
 public class Routine {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @ManyToOne
-    @NotNull
-    @JoinColumn(name = "user_id")
-    private SiteUser siteUser;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotNull
-    @Column
-    private String routine_name;
+  @ManyToOne
+  @NotNull
+  @JoinColumn(name = "user_id")
+  private SiteUser siteUser;
 
-    @OneToMany(mappedBy = "routine"  , cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Workout> workouts;
 
-    public Routine(){}
+  @Column
+  private String date;
 
-    public Routine(SiteUser siteUser, String routine_name) {
-        this.siteUser = siteUser;
-        this.routine_name = routine_name;
-    }
+  @NotNull
+  @Column
+  private String routine_name;
+
+  @OneToMany(mappedBy = "routine", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Workout> workouts;
+
+  public Routine() {
+  }
+
+  public Routine(SiteUser siteUser, String date, String routine_name) {
+    this.date = date;
+    this.siteUser = siteUser;
+    this.routine_name = routine_name;
+  }
 }
