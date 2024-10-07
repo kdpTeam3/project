b57cf02d7265053f5751eb1a9e172075de41c2fc
kitkat <mainist@skkukdp.re.kr>
2024-10-07 15:19:33 +0900

    메세지 URL 인코딩 처리

--- src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
+++ src/main/java/com/mysite/sbb/UserMatching/UserMatchingController.java
@@ -1,19 +1,20 @@
 package com.mysite.sbb.UserMatching;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 @PreAuthorize("isAuthenticated()")
 public class UserMatchingController {
     @GetMapping("/matching")
     public String showMatchingPage(Model model) {
-        RestTemplate restTemplate = new RestTemplate();
-        String formHtml = restTemplate.getForObject("http://localhost:8000/matching-form", String.class);
-        model.addAttribute("matchingForm", formHtml);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userId = auth.getName(); // 현재 로그인한 사용자의 ID를 가져옵니다.
+        model.addAttribute("userId", userId);
         return "UserMatchingPage";
     }
 }
