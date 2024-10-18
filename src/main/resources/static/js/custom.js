9adb3905d7122eb96f9b3d74973f20bd6f5d15a4
kitkat <mainist@skkukdp.re.kr>
2024-10-18 17:41:06 +0900

    탬플릿 파일 정리, 부트 스트랩 버전 일부 통일(운동 가이드까지 완료)

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,31 +1,29 @@
-
-  $(function () {
-
+$(function () {
     // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
+    $(".navbar-collapse a").on("click", function () {
+        $(".navbar-collapse").collapse("hide");
     });
 
     // AOS ANIMATION
     AOS.init({
-      disable: 'mobile',
-      duration: 800,
-      anchorPlacement: 'center-bottom'
+        disable: "mobile",
+        duration: 800,
+        anchorPlacement: "center-bottom",
     });
 
-
     // SMOOTHSCROLL NAVBAR
-    $(function() {
-      $('.navbar a, .hero-text a').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });    
-  });
-
-
-    
-
+    $(function () {
+        $(".navbar a, .hero-text a").on("click", function (event) {
+            var $anchor = $(this);
+            $("html, body")
+                .stop()
+                .animate(
+                    {
+                        scrollTop: $($anchor.attr("href")).offset().top - 49,
+                    },
+                    1000
+                );
+            event.preventDefault();
+        });
+    });
+});
